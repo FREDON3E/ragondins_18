9debfaa450cf1a1eb14738bd403029a0c6175e9a
FREDON3E <130762655+FREDON3E@users.noreply.github.com>
2025-02-25 17:12:16 +0100

    Delete data directory

--- data/PrfectureduCher_7.js
+++
@@ -1,1 +0,0 @@
-var json_PrfectureduCher_7 = {"type":"FeatureCollection","name":"PrfectureduCher_7","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"5","Nom":"Bourges"},"geometry":{"type":"Point","coordinates":[2.391127678161991,47.087221102600559]}}]}
